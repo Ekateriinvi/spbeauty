6e66d7b02fb02ae541cc824eba8c789e1fb580ad
Ekaterinvi <168282385+Ekateriinvi@users.noreply.github.com>
2024-04-28 19:10:43 +0000

    изменила стили кнопок инпутов и селектора

--- src/Components/TitledTextInput.js
+++ src/Components/TitledTextInput.js
@@ -2,17 +2,20 @@ import styled from "styled-components";
 
 const Input = styled.input`
     outline: none;
-    border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
-    color: #666787;
+    min-width: 100%;
+    border-radius: 24px;
+    border: solid #E2E8F0 1px;
+    background-color: #F8FAFC;
+    color: #0F172A;
     &:hover {
-        background-color: #D9E2EFB8;
+        background-color: #F8FAFC
+        opacity: 80%;
+        color: #94A3B8;
     }
 `;
 
 function TitledTextInput({title, def}) {
-    const Title = title === undefined ? 
+    const Title = HTMLTableCaptionElement === undefined ? 
         (<div></div>) : 
         (<div>{title}</div>);
     return (
